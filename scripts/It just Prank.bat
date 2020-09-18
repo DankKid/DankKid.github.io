20301b2ca48d39b836e52ac5b6ff71e65fe4e823
DankKid <61367825+DankKid@users.noreply.github.com>
2020-09-18 14:49:03 -0400

    Add files via upload

---
+++ scripts/It just Prank.bat
@@ -0,0 +1,74 @@
+@echo off
+color 0a
+title Setup Wizard
+echo    *** Do Not Open Any New Programs During Setup! ***
+echo    *** Do Not Restart or Power Down Your Machine! ***
+
+echo    Downloading Update      Please Wait
+echo ---------------------------------------
+echo                                 =   0 ]
+echo ---------------------------------------
+ping localhost -n 10 >nul
+cls
+echo    *** Do Not Open Any New Programs During Setup! ***
+echo    *** Do Not Restart or Power Down Your Machine! ***
+
+echo    Configuring Setup       Please Wait
+echo ---------------------------------------
+echo [][]                            =  15 ]
+echo ---------------------------------------
+ping localhost -n 8 >nul
+cls
+echo    *** Do Not Open Any New Programs During Setup! ***
+echo    *** Do Not Restart or Power Down Your Machine! ***
+
+echo    Installing Update       Please Wait
+echo ---------------------------------------
+echo [][][][]                        =  30 ]
+echo ---------------------------------------
+ping localhost -n 18 >nul
+cls
+echo    *** Do Not Open Any New Programs During Setup! ***
+echo    *** Do Not Restart or Power Down Your Machine! ***
+
+echo    Configuring Framework   Please Wait
+echo ---------------------------------------
+echo [][][][][]                      =  42 ]
+echo ---------------------------------------
+ping localhost -n 22 >nul
+cls
+echo    *** Do Not Open Any New Programs During Setup! ***
+echo    *** Do Not Restart or Power Down Your Machine! ***
+
+echo    Running Diagnostic      Please Wait
+echo ---------------------------------------
+echo [][][][][][][][]                =  50 ]
+echo ---------------------------------------
+ping localhost -n 5 >nul
+cls
+echo    *** Do Not Open Any New Programs During Setup! ***
+echo    *** Do Not Restart or Power Down Your Machine! ***
+
+echo    Booting                  Please Wait
+echo ---------------------------------------
+echo [][][][][][][][][][][][]        =  75 ]
+echo ---------------------------------------
+ping localhost -n 13 >nul
+cls
+echo    *** Do Not Open Any New Programs During Setup! ***
+echo    *** Do Not Restart or Power Down Your Machine! ***
+
+echo    Finishing Up            Please Wait
+echo ---------------------------------------
+echo [][][][][][][][][][][][][][][]  =  96 ]
+echo ---------------------------------------
+ping localhost -n 50 >nul
+cls
+echo    JK It was a Joke         .Complete.
+echo ---------------------------------------
+echo [][][][][][][][][][][][][][][][]= 100 ]
+echo ---------------------------------------
+ping loaclhost -n 5 >nul
+cls
+
+pause >nul
